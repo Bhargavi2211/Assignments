0a8da3dd5759f2a15fc33da09a1c445995aa271b
Bhargavi <62302058+Bhargavi2211@users.noreply.github.com>
2020-04-11 02:37:43 +0530

    Update Server_4.py

--- Server_4.py
+++ Server_4.py
@@ -2,18 +2,16 @@ import socket
 import threading
 
 class Server:
-    host = socket.gethostname()
-    print("Server will start on host:", host)
-    port = 2606
-
     def __init__(self, host, port):
         self.host = host
         self.port = port
         self.s = socket.socket()
         self.s.bind((host, port))
+        print("Server done binding to host and port successfully")
+        print("Server is waiting for connection")
         self.s.listen(2)
         self.conn, addr = self.s.accept()
-        print("Connection from:", addr[0], addr[1])
+        print(addr, "Has connected to server and online")
         self.t1 = threading.Thread(target=self.incoming_message)
         self.t2 = threading.Thread(target=self.send)
         self.t1.start()
@@ -34,3 +32,10 @@ class Server:
             self.conn.send(message.encode())
             if message == "Bye Client":
                 break
+
+host = socket.gethostname()
+print("Server will start on host:", host)
+port = 2606
+
+object = Server(host, port)
+
